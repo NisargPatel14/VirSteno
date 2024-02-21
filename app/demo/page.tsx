3e4b293448feec772448a1be3967ba9f51861edb
Lazycoder26 <Rudra26_2004@outlook.com>
2024-02-21 22:56:46 +0530

    Refactor page component and layout

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <main className='flex justify-center items-center h-screen'>
-        <div className='card-div w-1/2'>
-        <div className='card-1 w-3/5 h-2/5 bg-slate-400 p-5'>hello</div>
-        <div className='card-2'></div>
+    <main
+      className="flex flex-col justify-center items-center"
+      style={{ height: "calc(100vh - 160px)" }}
+    >
+      <div className="card-div flex justify-center items-center h-full w-1/2">
+        <div className="card-1 bg-slate-400 m-4 p-8 rounded-lg border-2 border-gray-300">
+          <h2 className="text-2xl font-bold mb-4">Card 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id
+            felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed
+            aliquam dictum sapien, id sagittis augue malesuada eu.
+          </p>
         </div>
+        <div className="card-2 bg-slate-400 m-4 p-8 rounded-lg border-2 border-gray-300">
+          <h2 className="text-2xl font-bold mb-4">Card 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id
+            felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed
+            aliquam dictum sapien, id sagittis augue malesuada eu.
+          </p>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default page;
+export default Page;
