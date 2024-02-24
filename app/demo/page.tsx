94a0e7dc0c65a164658a8709f13ec1ccbbefadee
Nisarg Patel <nisargpatel2101@gnu.ac.in>
2024-02-24 20:01:22 +0530

    Refactor card styles

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
       style={{ height: "calc(100vh - 160px)" }}
     >
       <div className="card-div flex justify-center items-center h-full w-1/2">
-        <div className="card-1 bg-slate-400 m-4 p-8 rounded-lg border-2 border-gray-300">
+        <div className="card-1 m-4 p-8 border-2 bg-white shadow-lg rounded-2xl">
           <h2 className="text-2xl font-bold mb-4">Card 1</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id
@@ -15,7 +15,7 @@ const Page = () => {
             aliquam dictum sapien, id sagittis augue malesuada eu.
           </p>
         </div>
-        <div className="card-2 bg-slate-400 m-4 p-8 rounded-lg border-2 border-gray-300">
+        <div className="card-2 m-4 p-8 border-2 bg-white shadow-lg rounded-2xl">
           <h2 className="text-2xl font-bold mb-4">Card 2</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id
