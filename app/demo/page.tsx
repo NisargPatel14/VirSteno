4ebdffc5742daf592316b078170ce398f71f3ad5
Nisarg Patel <nisargpatel2101@gnu.ac.in>
2024-02-25 11:23:11 +0530

    Update page layout and add video/audio transcription and summarization format

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -6,22 +6,42 @@ const Page = () => {
       className="flex flex-col justify-center items-center"
       style={{ height: "calc(100vh - 160px)" }}
     >
-      <div className="card-div flex justify-center items-center h-full w-1/2">
+      <div className="card-div flex justify-center items-center h-full w-3/4">
         <div className="card-1 m-4 p-8 border-2 bg-white shadow-lg rounded-2xl">
-          <h2 className="text-2xl font-bold mb-4">Card 1</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id
-            felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed
-            aliquam dictum sapien, id sagittis augue malesuada eu.
-          </p>
+          <h2 className="text-2xl font-bold mb-4">
+            Video Transcription and Summarization
+          </h2>
+          <input placeholder="Upload video" type="file" />
+          <textarea
+            name="videoTranscript"
+            placeholder="Transcribed text"
+            className="w-full border-2 rounded-lg p-3 mb-4 mt-4"
+            rows={5}
+          />
+          <textarea
+            name="videoSummary"
+            placeholder="Summarized text"
+            className="w-full border-2 rounded-lg p-3"
+            rows={5}
+          />
         </div>
         <div className="card-2 m-4 p-8 border-2 bg-white shadow-lg rounded-2xl">
-          <h2 className="text-2xl font-bold mb-4">Card 2</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id
-            felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed
-            aliquam dictum sapien, id sagittis augue malesuada eu.
-          </p>
+          <h2 className="text-2xl font-bold mb-4">
+            Audio Transcription and Summarization
+          </h2>
+          <input placeholder="Upload audio" type="file" />
+          <textarea
+            name="audioTranscript"
+            placeholder="Transcribed text"
+            className="w-full border-2 rounded-lg p-3 mb-4 mt-4"
+            rows={5}
+          />
+          <textarea
+            name="audioSummary"
+            placeholder="Summarized text"
+            className="w-full border-2 rounded-lg p-3"
+            rows={5}
+          />
         </div>
       </div>
     </main>
