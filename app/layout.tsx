0eae4d92fb4f45545d5b8a972a4cde25c4f9e6c2
NisargPatel99 <139237451+NisargPatel14@users.noreply.github.com>
2024-02-26 12:07:25 +0530

    Update copyright year to 2024

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,7 +93,7 @@ export default function RootLayout({
               height={60}
               priority
             />
-            <span className="text-sm">© 2023</span>
+            <span className="text-sm">© 2024</span>
             <nav className="flex justify-end grow sm:gap-2">
               <a
                 className="flex gap-2 px-3 py-2 text-sm font-semibold text-gray-600 transition duration-100 rounded-md hover:text-gray-800"
